ea7d626acd7c68022f861cd41ca232316f3a08cf
CHIBA YUUMA <obakasann1819@gmail.com>
2026-04-22 22:12:28 +0900

    自作でusestateの挙動確認とボタンを作った

--- udemy react復習/react-guide-material/05_state_and_event/src/020_useState/start/Example.jsx
+++ udemy react復習/react-guide-material/05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,8 +1,15 @@
-const Example = () => {
+import { useState } from "react";
 
+
+const Example = () => {
+  let [val, setVal] = useState()
+  console.log ('再レンダリングされました')
   return (
     <>
-      
+      <input type="text"  onChange={(e)=>{
+        console.log(e.target.value);
+        setVal(e.target.value);
+        val}}/> = {val}
     </>
   );
 };
--- udemy react復習/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ udemy react復習/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+  let [setA,setCountA] = useState(0);
+  const handleClick =()=>{
+    setCountA(setA+1)
+  }
+  console.log('再レンダリングされました')
+  return(
+    <>
+    <h1>ボタンが{setA}回クリックされました。</h1>
+    <button onClick={handleClick}>ボタンA</button>
+    </>
+  )
 };
 
 export default Example;
