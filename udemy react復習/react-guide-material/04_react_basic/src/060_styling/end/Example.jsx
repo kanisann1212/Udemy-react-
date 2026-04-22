3126558fab2cd607adf630cad9d9b78c2646c8b0
yuuma <obakasann1819@gmail.com>
2026-04-22 13:02:51 +0900

    udemy学習の記録

--- udemy react復習/react-guide-material/04_react_basic/src/060_styling/end/Example.jsx
+++ udemy react復習/react-guide-material/04_react_basic/src/060_styling/end/Example.jsx
@@ -3,20 +3,11 @@
 */
 
 //  POINT CSS ファイルを import する
-import "./Example.css";
+import Child from "../start/coponent/Child";
 
 const Example = () => {
   return (
-    /* POINT クラス名は class ではなく className で指定する
-    HTML のように見えますが、これは JSX と呼ばれる構文です。class 属性は className を使用しましょう。
-    */
-  
-    /* POINT class と className
-    React 16 以降では custom DOM attributes をサポートしているため class も使用可能です。（ただ、ウォーニングも出るため、非推奨！！）
-    */
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+ <Child />
   );
 };
 
---
+++ udemy react復習/react-guide-material/04_react_basic/src/060_styling/start/coponent/Child.jsx
@@ -0,0 +1,8 @@
+   export default function Child(props){
+    return(
+    <div className="component">
+      <h3>Hello {props.name}</h3>
+    </div>
+    )
+   }
+    
