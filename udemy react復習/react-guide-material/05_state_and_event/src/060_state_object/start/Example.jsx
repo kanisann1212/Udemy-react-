c1c9b0d4ed9e91629890fa3b3f0b32959f5d981a
yuuma <obakasann1819@gmail.com>
2026-04-23 17:55:29 +0900

    オブジェクトの操作の勉強

--- udemy react復習/react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
+++ udemy react復習/react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person,setPerson] = useState(personObj)
+  const setName =(e)=>{
+    setPerson({...person,name:e.target.value})
+  }
+  const setAge= (e)=>{
+    setPerson({...person,age:e.target.value})
+  }
+  return(
+    <>
+    <h1>NAME:{person.name}</h1>
+    <h1>AGE:{person.age}</h1>
+    <input type="text" value={person.name} onChange={setName}></input>
+    <input type="number" value={person.age} onChange={setAge}></input>
+    </>
+  )
 };
 
 export default Example;
