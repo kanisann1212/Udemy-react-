5d011f0eeb353bca99efae707f53aeb73612fbc4
yuuma <obakasann1819@gmail.com>
2026-04-23 17:55:05 +0900

    配列の数字の入れ替えの勉強

--- udemy react復習/react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
+++ udemy react復習/react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [num,setNum]=useState(numArray)
+  const shuffle =()=>{
+    const newnum =[...num]
+    const value = newnum.pop()
+    newnum.unshift(value);
+    setNum(newnum);
+  }
 
   return (
     <>
-      
+      <h1>{num}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
