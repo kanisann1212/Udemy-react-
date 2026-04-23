f3dfb6fc7132621e44da18b6bf44e99239c40162
yuuma <obakasann1819@gmail.com>
2026-04-23 09:27:45 +0900

    クリックで増減する簡単なボタンの作成

--- udemy react復習/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
+++ udemy react復習/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [set,setCount]= useState(0);
+  const countUp =()=>{
+    setCount(set+1);
+  }
+  const countDown =()=>{
+    setCount(set-1);
+  }
+  return(
+    <>
+    <h1>現在のカウント：{set}</h1>
+    <button onClick={countUp}>+</button>
+    <button onClick={countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
