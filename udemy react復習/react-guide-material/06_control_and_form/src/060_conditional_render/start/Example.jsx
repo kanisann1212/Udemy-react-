db4812bde6c8e2a3216109bdce172f15bf36e183
CHIBA YUUMA <obakasann1819@gmail.com>
2026-04-28 20:15:56 +0900

    条件分岐の勉強

--- udemy react復習/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ udemy react復習/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null,"Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,12 +15,14 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalstr=animal ?? "";
+            const isMatch = animalstr.indexOf(filterVal) !== -1;
             return isMatch;
           })
+          // {}を書く場合は処理を書くとき(この場合はreturnを最後に書かないと値が返ってこない)
+          // ()を書くときはそのまま値を返す時
           .map((animal) => (
-            <li key={animal}>{animal}</li>
+            <li key={animal}>{animal ?? "null,undefinedでした"}{animal === "Dog"&&"犬だよ"}</li>
           ))}
       </ul>
     </>
