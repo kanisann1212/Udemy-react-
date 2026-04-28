5cff3c0adc36ad5d993db3a62a2e10da20c9f94b
CHIBA YUUMA <obakasann1819@gmail.com>
2026-04-28 19:45:55 +0900

    北海道旅行分の勉強の総復習(４・２４～４・２６)

--- udemy react復習/react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
+++ udemy react復習/react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -2,12 +2,14 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  
+  // const animalList =
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {animals.map((animal)=> <li key={animal}>helloo,{animal}</li>)}
       </ul>
     </>
   );
--- udemy react復習/react-guide-material/06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ udemy react復習/react-guide-material/06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterval,setFilterval] =useState("");
   return (
     <>
+    <input type="text" value={filterval} onChange={(e)=>setFilterval(e.target.value)}/>
       <h3>配列のフィルター</h3>
       <ul>
         {animals
-          .map((animal) => (
+            // ↓↓↓メソッドチェーンが使われている↓↓↓  
+          .filter(animal=>animal.indexOf(filterval) !==-1)
+          .map((animal) => (      
           <li>{animal}</li>
         ))}
       </ul>
