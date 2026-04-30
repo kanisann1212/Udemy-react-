eb2d6ed992261c1e71c6cae32e842a82c320aa11
CHIBA YUUMA <obakasann1819@gmail.com>
2026-04-30 19:48:37 +0900

    ラジオボタンの使い方

--- udemy react復習/react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
+++ udemy react復習/react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setfruit] = useState("Apple")
+  const onChange = (e) => setfruit(e.target.value)
+  const fruitgroupe = ["Apple", "Orange", "Cherry"];
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {/* ↓普通の書き方 */}
+      {/* <label>
+        <input type="radio" value="Apple" checked={fruit === "Apple"} onChange={onChange} />
+        リンゴ
+      </label>
+      <label>
+        <input type="radio" value="Orange" checked={fruit === "Orange"} onChange={onChange} />
+        オレンジ
+      </label>
+      <label>
+        <input type="radio" value="Cherry" checked={fruit === "Cherry"} onChange={onChange} />
+        さくらんぼ
+      </label> */}
+      <h1>わたしは{fruit}がたべたい</h1>
+      
+      {/* ↓mapメソッドを使ったかきかた */}
+      {fruitgroupe.map(value => {
+        return (
+          <label key={value}>
+            <input type="radio" value={value} checked={fruit === value} onChange={onChange} />
+            {value}
+          </label>
+        )
+      })}
+    </>
   );
 };
 
