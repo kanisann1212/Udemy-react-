db2591491a0b3d0b6960b6a9d08707eca9aa21d1
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-01 17:48:56 +0900

    マルチチェックボックスの学習記録

--- udemy react復習/react-guide-material/06_control_and_form/src/110_multi_checkbox/start/Example.jsx
+++ udemy react復習/react-guide-material/06_control_and_form/src/110_multi_checkbox/start/Example.jsx
@@ -2,66 +2,43 @@ import { useState } from "react";
 
 // POINT 複数チェックボックスの実装
 const Example = () => {
-  const [fruits, setFruits] = useState([
-    { label: "Apple", value: 100, checked: false },
-    { label: "Banana", value: 200, checked: false },
-    { label: "Cherry", value: 300, checked: false },
+  const [fruits,setFruits] = useState([
+    {label:"Apple",value:100,chcked:false},
+    {label:"Banana",value:200,chcked:false},
+    {label:"Cherry",value:300,chcked:false}
   ]);
+const [num,setnum]=useState(0);
+const handeleChange =(e)=>{
+  const newFruits= fruits.map(fruit=>{
+    const newFruit={...fruit}
+    if(e.target.value===newFruit.label){
+      newFruit.chcked=!fruit.chcked;
+    }
+    return newFruit;
+  })
+  setFruits(newFruits);
+  let sumVal = 0;
+  newFruits.forEach(fruit=>{
+    if(fruit.chcked){
+      sumVal +=fruit.value
+    }
+  })
+  setnum(sumVal)
+}
 
-  const [sum, setSum] = useState(0);
-
-  const handleChange = (e) => {
-    const newFruits = fruits.map((fruit) => {
-      const newFruit = { ...fruit };
-      if (newFruit.label === e.target.value) {
-        newFruit.checked = !fruit.checked;
-      }
-
-      return newFruit;
-    });
-
-    setFruits(newFruits);
-    // forEachバージョン
-    // let sumVal = 0;
-    // newFruits.forEach(fruit => {
-    //   if(fruit.checked) {
-    //     sumVal = sumVal + fruit.value;
-    //   }
-    // });
-
-    // filter + forEachバージョン
-    // let sumVal = 0;
-    // newFruits
-    //   .filter((fruit) => fruit.checked)
-    //   .forEach((fruit) => (sumVal = sumVal + fruit.value));
-
-    // filter + reduceバージョン
-    let sumVal = newFruits
-      .filter((fruit) => fruit.checked)
-      .reduce((sumVal, fruit) => sumVal + fruit.value, 0);
-    setSum(sumVal);
-  };
-  return (
-    <div>
-      {fruits.map((fruit) => {
-        return (
-          <div key={fruit.label}>
-            <input
-              id={fruit.label}
-              type="checkbox"
-              value={fruit.label}
-              checked={fruit.checked}
-              onChange={handleChange}
-            />
-            <label htmlFor={fruit.label}>
-              {fruit.label}:{fruit.value}
-            </label>
-          </div>
-        );
-      })}
-      <div>合計：{sum}</div>
-    </div>
-  );
+return(
+  <div >
+    {fruits.map(fruit=>{
+      return(
+        <div key={fruit.label}>
+        <input type="checkbox" value={fruit.label}  onChange={handeleChange} id={fruit.label} checked={fruit.chcked}/>
+        <label htmlFor={fruit.label}>{fruit.label}:{fruit.value}円</label>
+        </div>
+      )
+    })}
+    <h1>合計料金:{num}</h1>
+  </div>
+)
 };
 
 export default Example;
