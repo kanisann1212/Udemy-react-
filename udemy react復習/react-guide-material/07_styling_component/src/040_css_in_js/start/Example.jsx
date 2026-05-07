f70c3f6f47caddcf94779335102f5cfeac75b77d
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-07 14:21:23 +0900

    お昼の時間・styled.componetsの勉強

--- udemy react復習/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.jsx
+++ udemy react復習/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
-
+import styled from "styled-components"
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
+  const Styledbutton = styled.button`
+    margin: auto;
+    border-radius: 9999px;
+    border: none;
+    display: block;
+    width: 120px;
+    height: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    background: ${({props}) => props ? 'pink': ''}
+    `;
+    const Aquabutton = styled(Styledbutton)`
+      background-color: aqua;
+
+      &:hover { 
+        color: red;
+      }
+    `;
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
@@ -13,6 +31,8 @@ const Example = () => {
       >
         ボタン
       </button>
+      <Styledbutton props={isSelected} onClick={clickHandler}>二個目のボタン</Styledbutton>
+      <Aquabutton>こんにちわ</Aquabutton>
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
