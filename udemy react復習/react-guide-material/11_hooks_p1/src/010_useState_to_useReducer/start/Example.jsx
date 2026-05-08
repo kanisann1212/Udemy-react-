22993ee5b52ef34f39886c209747a1f834e0cc7b
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-08 15:00:39 +0900

    useReducerの勉強

--- udemy react復習/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ udemy react復習/react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,44 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  // ※更新関数を第一引数に、第二引数に初期値
+  const [rstate, dispatch] = useReducer((prev, {type,step}) => {
+    // switch(action){
+    //   case '+':
+    //     return ++prev
+    //   case '-':
+    //     return --prev
+    //   defalut :
+    //   throw new Error('不明な値です。')  
+    // }　
+    // ↑こうやっても書ける
+    if (action === '+') {
+      return prev + step;
+    } else if (action === '-') {
+      return prev - step;
+    }
+  }, 0)
+ // ↑dispatchからstepとして引数も受け取れる
+ 
+  const Rplus = () => {
+    dispatch({type:'+',step:2});
+  }
+  const Rminus = () => {
+    dispatch({type:'-',step:3});
+  }
+  const plusState = () => {
+    setState(prev => ++prev)
+  }
+
   return (
     <>
+      <h1>Usestateの場合：{state}</h1>
+      <button onClick={plusState}>+</button>
+      <h1>UseReducerの場合：{rstate}</h1>
+      <button onClick={Rplus}>+</button>
+      <button onClick={Rminus}>-</button>
     </>
   );
 };
