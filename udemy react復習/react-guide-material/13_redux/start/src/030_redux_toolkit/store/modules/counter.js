f8e0e60657b6ebde887586a915ab27f5f8bff333
yuuma <obakasann1819@gmail.com>
2026-05-12 13:29:31 +0900

    redux toolkitの練習

--- udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/modules/counter.js
+++ udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/modules/counter.js
@@ -1,28 +1,20 @@
-const initialState = 0;
+import { createSlice } from "@reduxjs/toolkit";
 
-const reducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "counter/+":
-      return state + payload;
-    case "counter/-":
-      return state - payload;
-    default:
-      return state;
-  }
-};
-
-const add = (payload) => {
-    return {
-        type: "counter/+",
-        payload
+const reducerTest = createSlice({
+  name:'counter',
+  initialState: 0,
+  reducers:{
+    add(state,{payload}){
+      return state + payload
+    },
+    minus(state, {payload}){
+      return state - payload
     }
-}
+  },
+})
 
-const minus = (payload) => {
-    return {
-        type: "counter/-",
-        payload
-    }
-}
-
-export { reducer, add, minus }
+const { add, minus } =reducerTest.actions
+export { add, minus }
+export default reducerTest.reducer
+// ↑作ったスライスのルールブック(reducer)だけ取りだせればよいので.reducerで取り出して渡している。 
+// ちなみにexport defaultは呼び出した側(import)側で名前を決めれる
