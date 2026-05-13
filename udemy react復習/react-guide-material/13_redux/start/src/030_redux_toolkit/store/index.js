0faddc223a53f1a767cf1d8d26e35cc8397ba769
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-13 14:11:58 +0900

    reDUx勉強

--- udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/index.js
+++ udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/index.js
@@ -1,12 +1,10 @@
-import { reducer } from "./modules/counter"
 import { configureStore} from "@reduxjs/toolkit"
 import reducer from "./modules/counter";
 
 
-export default createStore(reducers);
-
 export default configureStore({
   reducer:{
-    counter:reducer
+    counter: reducer
+    // ↑これには好きなプロパティ名とスライスから取り出したreducerを紐付けて登録する
   }
 });
--- udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/modules/counter.js
+++ udemy react復習/react-guide-material/13_redux/start/src/030_redux_toolkit/store/modules/counter.js
@@ -2,13 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const reducerTest = createSlice({
   name:'counter',
-  initialState: 0,
+  initialState: {count:0},
+  // ↑
+  // stateがオブジェクト・配列など不変性を保持するための記述をしなけばならないときはreduxは純粋関数でなければいけないのでしっかり記述する
+  // ↓
   reducers:{
     add(state,{payload}){
-      return state + payload
+      state.count = state.count + payload
+      // ↑Immerライブラリのおかげで不変性を損なう書き方でもtoolkitではできる
+      // const newState = {...state}
+      // newState.count = state.count + payload
+      // return newState
     },
     minus(state, {payload}){
-      return state - payload
+      state.count = state.count - payload
+      // ↑Immerライブラリのおかげで不変性を損なう書き方でもtoolkitではできる
+      // const newState = {...state}
+      // newState.count = state.count - payload
+      // return newState
     }
   },
 })
@@ -18,3 +29,4 @@ export { add, minus }
 export default reducerTest.reducer
 // ↑作ったスライスのルールブック(reducer)だけ取りだせればよいので.reducerで取り出して渡している。 
 // ちなみにexport defaultは呼び出した側(import)側で名前を決めれる
+// また複数スライスを作成するときはスライスごとにファイルを分けるのが慣習
