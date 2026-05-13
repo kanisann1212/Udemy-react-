e11ca8bf0b89003712782dd5568e9ebb6828d8bb
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-13 23:17:45 +0900

    useMemoのこと。勉強

--- udemy react復習/react-guide-material/15_performance/src/020_memo/start/child.jsx
+++ udemy react復習/react-guide-material/15_performance/src/020_memo/start/child.jsx
@@ -8,6 +8,7 @@ function areEqual(prev,next){
   }
 }
 // 第二引数に関数を渡せる。if文などを使って更新前と後の数値を比較してtrueなら再レンダリングなし、falseなら再レンダリングありの挙動をとる
+// memoはコンポーネントのみメモ化
 
  const ChildMemo = memo(({stateB}) => {
   console.log('子供');
--- udemy react復習/react-guide-material/15_performance/src/060_useTransition/start/Example.jsx
+++ udemy react復習/react-guide-material/15_performance/src/060_useTransition/start/Example.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 const generateDummyItem = (num) => {
   return new Array(num).fill(null).map((item, index) => `item ${index}`);
 };
+//↑ arryで引数分の要素が入った配列作る→fillでnullでうめる→mapでそのnullにitem{index}といれる
 
 const dummyItems = generateDummyItem(10000);
-
 const Example = () => {
   const [filterVal, setFilterVal] = useState("");
-
+  const [isPending, startTransition] = useTransition();
   const changeHandler = (e) => {
-    setFilterVal(e.target.value);
+    startTransition(() => {
+      setFilterVal(e.target.value);
+    })
   };
 
   return (
     <>
       <input type="text" onChange={changeHandler} />
+      {isPending && <h1>ロード中</h1>}
       <ul>
         {dummyItems
           .filter((item) => {
@@ -25,6 +28,7 @@ const Example = () => {
           .map((item) => (
             <li key={item}>{item}</li>
           ))}
+        {/* ↑ filterで最初に要素を全表示。入力値が変わったらstate更新してstateの入力値が含まれるものを表示 */}
       </ul>
     </>
   );
