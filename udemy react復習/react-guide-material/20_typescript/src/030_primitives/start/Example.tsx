fa061cab1f584fe080d3aec69121f950a4f21b84
yuuma <obakasann1819@gmail.com>
2026-05-15 16:44:55 +0900

    Type scriptの勉強をしたにょん

--- udemy react復習/react-guide-material/20_typescript/src/030_primitives/start/Example.tsx
+++ udemy react復習/react-guide-material/20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,18 @@
 const Example = () => {
   // 練習用
+  const str :string = 'unko'
+  console.log(str)
+
+  const num :number = 84848
+  console.log(num)
+
+  const bool :boolean = true
+  console.log(bool)
+
+  // リテラル型:下記のように型宣言のとこに明示的に値を入れるとその値しか受け付けない型を定義できる
+  let isis :true = true
+  let numis : 5757 = 5757
+  console.log(isis,numis)
 };
 
 export default Example;
