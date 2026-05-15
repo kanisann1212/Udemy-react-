3a08b13042f13a2f67158e037c4e8ec93a5383f9
yuuma <obakasann1819@gmail.com>
2026-05-15 17:12:06 +0900

    ＴＳのお勉強パート２

--- udemy react復習/react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
+++ udemy react復習/react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,23 @@
 const Example = () => {
   // 練習用
+  let SorN : string | number = 3232
+  SorN = 'tyutyu'
+  // ユニオン型は何個でも定義できる( | )は見栄えの為で、極論要らない。リテラル型もいける 
+
+  let unko : 123 | 'hello' | string = 123
+  console.log (unko)
+
+  // type week = 
+  // | 'monday'
+  // | 'tuesday'
+
+  type monster = 
+  'unko' 
+  'tinko'
+  'manko'
+
+  const day : monster = 'unko'
+  console.log(day)
 };
 
 export default Example;
