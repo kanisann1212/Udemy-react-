3282d6958d2bb0dbfebb7a5a68c48734bc3183f5
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-15 14:37:10 +0900

    サーバーアクションの仕組みを勉強した。お昼

--- udemy react復習/react-guide-material/21_nextjs_app_router/start/src/actions/createItem.js
+++ udemy react復習/react-guide-material/21_nextjs_app_router/start/src/actions/createItem.js
@@ -1,6 +1,35 @@
-
+'use server'
 import { ENDPOINT } from "@/constants";
 
-export async function createItem(formData) {
-  
+
+export async function createItem(state,formData) {
+  // ↑useFormStateを使う場合はサーバアクションの関数の第一引数にstateを設定する
+  const id = formData.get('id')
+  const title = formData.get('title')
+
+  if (id === '' || title === '') {
+    return { msg: '入力フィールドが空です。' }
+  }
+
+  try {
+    const res = await fetch(ENDPOINT, {
+      method: 'POST',
+      // ↑どんなことをするのかを指定する
+      headers: {
+        "Content-Type": "application/json"
+        // ↑どういうデータ形式で送るのかしているする
+      },
+      body: JSON.stringify({ id, title })
+      // ↑どういうデータを送るのかを指定する
+    })
+    const data = await res.json()
+    // ↑POSTをするとちゃんと登録しました！って言った感じの確認書が返ってくる(登録した内容とサーバーが付け加えた内容が入ってる)のでそれをjson➡jsオブジェクトに変換➡そのごresponseでjsonにして自分のコンポーネントに返してあげる
+    return { msg: `${data.id}:${data.title}の登録完了`}
+  } catch (e) {
+  return { msg: '登録に失敗しました' }
 }
+}
+
+
+// ※server actionはuse serverをファイルの先頭orコンポーネント内に記述するだけ、returnでRsponseをつかわずとも勝手にjsonに変換してかえしてくれる
+
--- udemy react復習/react-guide-material/21_nextjs_app_router/start/src/app/050_actions/components/form.js
+++ udemy react復習/react-guide-material/21_nextjs_app_router/start/src/app/050_actions/components/form.js
@@ -2,13 +2,19 @@
 
 import { createItem } from "@/actions/createItem";
 import { useState } from "react";
+import { useFormState } from "react-dom";
 
 export default function ArticleForm() {
   const [newId, setNewId] = useState();
   const [newTitle, setNewTitle] = useState();
 
+  useFormstateは
+  const [state,createItemAction] = useFormState(createItem, {msg:null} )
+  // ↑第一引数にサーバアクションで定義した関数を登録する
+  // ↑stateにはサーバアクションの関数の実行結果が入る
+
   return (
-    <form>
+    <form action={createItemAction}>
       <div>
         <label>
           {" "}
@@ -24,7 +30,7 @@ export default function ArticleForm() {
         </label>
       </div>
       <button type="submit">送信</button>
-      <div style={{ color: 'red' }}></div>
+      <div style={{ color: 'red' }}>{state.msg}</div>
     </form>
   );
 }
