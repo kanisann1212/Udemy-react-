f2debcfbc437dd1239fd7c063a002ecd0e6b573e
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-16 15:34:58 +0900

    TS ジェネリクス

--- udemy react復習/react-guide-material/20_typescript/src/090_generics/start/Example.ts
+++ udemy react復習/react-guide-material/20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,20 @@
 const Example = () => {
   // 練習用
+  const sum1 = (x: string, y: number): string[] => {
+    return new Array(y).fill(x);
+
+    const sum2 = (x: number, y: number): number[] => {
+      return new Array(y).fill(x);
+    }
+    // ↑上記のように引数や戻り値の型だけを変えて関数を使いまわしたいときに ジェネリクス をつかう
+
+    const GENERICS = <T>(x: T, y: number): T[] => {
+      return new Array(y).fill(x)
+    }
+    const TEST = GENERICS<number>(10, 3)
+    const TEST2 = GENERICS(10, 3)
+    console.log(TEST)
+  }
 };
 
 export default Example;
